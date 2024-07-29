6fb3df70f11cbdd6e0a63593ca232c061ccfc622
Pawan Sharma <pawan2.radhe@gmail.com>
2024-07-29 17:10:30 +0530

    uploaded school management system

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as router, Routes, Route, Link, BrowserRouter } from 'react-router-dom';
+import Signin from './signin';
+import Register from './register';
+import Main from './main';
+import Courses from './courses';
+import Students from './students';
+import Attendences from './attendences';
+import Schedules from './schedules'
+
+import Fees from './fees';
+import Holidays from './holidays';
+import Transports from './transports';
+import Notifications from './notifications';
+import Rightbar from './rightbar';
+import Gatepass from './gatepass';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Signin />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/main' element={<Main />} />
+          <Route path='/courses' element={<Courses />} />
+          <Route path='/students' element={<Students />} />
+          <Route path='/attendences' element={<Attendences />} />
+          <Route path='/schedules' element={<Schedules />} />
+          <Route path='/fees' element={<Fees />} />
+          <Route path='/holidays' element={<Holidays />} />
+          <Route path='/transports' element={<Transports />} />
+          <Route path='/notifications' element={<Notifications />} />
+          <Route path='/rightbar' element={<Rightbar />} />
+          <Route path='/gatepass' element={<Gatepass />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
